da1323b906caf0b04e677a1d4258f072e0c40b25
Greyson Wright <greysonmwright@gmail.com>
2017-12-02 16:23:40 -0600

    -initial commit

---
+++ cs457-final/cs457-final/final.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  cs457-final
+//
+//  Created by Greyson Wright on 12/2/17.
+//  Copyright © 2017 Greyson Wright. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	return 0;
+}
