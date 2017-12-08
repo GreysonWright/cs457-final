ca1f773fd2692429ee10cbd8eff7e8119851a8d7
Greyson Wright <greysonmwright@gmail.com>
2017-12-08 16:55:11 -0600

    - finished interpreter with bugs

--- cs457-final/cs457-final/final.c
+++ cs457-final/cs457-final/final.c
@@ -9,10 +9,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "DataBase.h"
 #include "Record.h"
 #include "darray.h"
 #include "scanner.h"
+
+char *stripWhiteSpace(char *);
+int parseVersion(char *);
 
 int main(int argc, const char * argv[]) {
 	DataBase *dataBase = newDataBase(displayRecord);
@@ -26,55 +30,60 @@ int main(int argc, const char * argv[]) {
 	line = 0;
 	
 	FILE *queryFile = fopen("/Users/gwright/Git/cs457-final/cs457-final/cs457-final/queries.txt", "r");
+	FILE *outFile = fopen("/Users/gwright/Git/cs457-final/cs457-final/cs457-final/gwright.txt", "w+");
 	line = readLine(queryFile);
+	line = stripWhiteSpace(line);
 	while (!feof(dataFile)) {
 		char *collectionCommand = strtok(line, "[");
 		if (strstr(collectionCommand, "final.")) {
 			char *query = strtok(0, "]");
 			if (strstr(collectionCommand, "count")) {
-				char *sepparator = strtok(0, "[");
+				(void)strtok(0, "[");
 				char *versionString = strtok(0, "]");
-				int version = 0;
-				
-				if (strcmp(sepparator, ")")) {
-					version = 1;
-				}
-				
-				if (strcmp(versionString, ")")) {
-					version = 0;
-				} else {
-					version = atoi(versionString);
-				}
-				
+				int version = parseVersion(versionString);
 				int count = countDataBase(dataBase, query, version);
+				fprintf(outFile, "count_%s:%d\n", query, count);
 			} else {
-				DArray *results = 0;
+				(void)strtok(0, "[");
+				char *fields = strtok(0, "]");
+				(void)strtok(0, "[");
+				char *versionString = strtok(0, "]");
+				int version = parseVersion(versionString);
+				DArray *results = newDArray(0);
 				if (strstr(collectionCommand, "query")) {
-//					results = queryDataBase(dataBase, query);
+					results = queryDataBase(dataBase, query, version);
 				} else if (strtok(collectionCommand, "sort")) {
-//					results = sortDataBase(dataBase, query);
+					results = sortDataBase(dataBase, query, version);
 				}
+				displaySelectDataBase(outFile, results, fields);
 			}
 		}
 		
 		line = readLine(dataFile);
 	}
-	fclose(queryFile);
-	
-	FILE *outFile = fopen("/Users/gwright/Git/cs457-final/cs457-final/cs457-final/gwright.txt", "w+");
 	fclose(outFile);
-//
-//	char *searchVal = "";
-//	char *resultFields = "";
-//	DataBase *dataBase = newDataBase(displayRecord);
-//	insertDataBase(dataBase, "DocID:1 test:1 a:0");
-//	insertDataBase(dataBase, "DocID:2 test:2");
-//	insertDataBase(dataBase, "DocID:1 test:2 a:1");
-//	insertDataBase(dataBase, "DocID:0 test:2");
-//	insertDataBase(dataBase, "DocID:3 test:2");
-//	insertDataBase(dataBase, "DocID:2 test:2");
-//	insertDataBase(dataBase, "DocID:2 test:2");
-//	DArray *results = sortDataBase(dataBase, "DocID", 1);
-//	displaySelectDataBase(stdout, results, 0);
+	fclose(queryFile);
 	return 0;
+}
+
+char *stripWhiteSpace(char *token) {
+	long tokenLen = strlen(token);
+	char *newToken = malloc(tokenLen);
+	int count = 0;
+	for (int i = 0; i < tokenLen; i++) {
+		if (!isspace(token[i])) {
+			newToken[count++] = token[i];
+		}
+	}
+	return newToken;
+}
+
+int parseVersion(char *versionString) {
+	if (versionString == 0) {
+		return 1;
+	}
+	if (strcmp(versionString, ")")) {
+		return 0;
+	}
+	return atoi(versionString);
 }
